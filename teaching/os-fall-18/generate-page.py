9e66ff7419aba65a825bef208a665ac4c86aea3d
nipunbatra <nipunbatra0@gmail.com>
2018-07-23 10:06:57 +0530

    Added schedule

--- teaching/os-fall-18/generate-page.py
+++ teaching/os-fall-18/generate-page.py
@@ -3,9 +3,16 @@ import pandas as pd
 
 NUM_COLUMNS = 4
 df = pd.read_csv("schedule.csv")
-df_cols = df.columns[:NUM_COLUMMNS]
+df_cols = df.columns[:NUM_COLUMNS]
 html_table = df[df_cols].to_html()
 
 with open('index.html','r') as f:
     s = f.read()
     
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(s,'html.parser')
+soup.find("div", {"id": "schedule"}).string=""
+soup.find("div", {"id": "schedule"}).append(BeautifulSoup(html_table, 'html.parser'))
+
+with open('index.html','w') as f:
+    f.write(str(soup.prettify()))
