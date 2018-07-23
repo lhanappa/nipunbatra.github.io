2fdc211e09061cf3a819b2344299694079c8dde8
nipunbatra <nipunbatra0@gmail.com>
2018-07-23 12:39:38 +0530

    Added

--- teaching/os-fall-18/generate-page.py
+++ teaching/os-fall-18/generate-page.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
 
 
-NUM_COLUMNS = 4
+NUM_COLUMNS = 5
 df = pd.read_csv("schedule.csv")
+df =df.replace(np.NaN, "")
 df_cols = df.columns[:NUM_COLUMNS]
 html_table = df[df_cols].to_html()
 
 with open('index.html','r') as f:
     s = f.read()
     
-from bs4 import BeautifulSoup
 soup = BeautifulSoup(s,'html.parser')
 soup.find("div", {"id": "schedule"}).string=""
 soup.find("div", {"id": "schedule"}).append(BeautifulSoup(html_table, 'html.parser'))
